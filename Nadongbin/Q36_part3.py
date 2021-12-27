f546bc4dc973f310920dea3829d392d53e14f028
paulleelife <bluewaves3141@gmail.com>
2021-12-27 21:01:27 +0900

    nadongbin 36

---
+++ Nadongbin/Q36_part3.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+str1 = input()
+str2 = input()
+
+# dp table initialize
+dp_arr = np.zeros([len(str1)+1, len(str2)+1], dtype=int)
+dp_arr[:, 0] = np.arange(len(str1)+1)
+dp_arr[0, :] = np.arange(len(str2)+1)
+
+for row in range(1, len(str1)+1):
+  for col in range(1, len(str2)+1):
+    if str1[row-1] == str2[col-1]:
+      dp_arr[row, col] = dp_arr[row-1, col-1]
+    else:
+      dp_arr[row, col] = 1 + min(dp_arr[row-1, col-1],
+                                 dp_arr[row-1, col], 
+                                 dp_arr[row, col-1])
+
+print(dp_arr[-1, -1])
